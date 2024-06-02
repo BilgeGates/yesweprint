f95e9becaf6685cd1e35033e3b4bf62740970abb
BilgeGates <huseynzadekhatai@gmail.com>
2024-06-02 21:35:05 +0400

    data.js added

---
+++ src/components/Navbar/data.js
@@ -0,0 +1,8 @@
+const data = [
+  { id: 1, link: "#", title: "Home" },
+  { id: 2, link: "#catologue", title: "Catologue" },
+  { id: 3, link: "#services", title: "Services" },
+  { id: 4, link: "#help", title: "Help" },
+];
+
+export default data;
