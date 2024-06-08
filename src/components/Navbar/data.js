4f56ea9940701ca2549fed2a18a16c65003e3924
BilgeGates <huseynzadekhatai@gmail.com>
2024-06-08 22:23:00 +0400

    project updated

--- src/components/Navbar/data.js
+++
@@ -1,8 +0,0 @@
-const data = [
-  { id: 1, link: "#catoluge", title: "Catoluge" },
-  { id: 2, link: "#services", title: "Services" },
-  { id: 3, link: "#help", title: "Help" },
-  { id: 4, link: "#design__studio", title: "Design Studio" },
-];
-
-export default data;
